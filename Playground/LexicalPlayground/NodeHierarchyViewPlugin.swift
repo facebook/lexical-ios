d8f9a5a6b38a69edace323f1625254a6483a3c5c
Amy Worrall <amyworrall@fb.com>
2023-04-05 20:32:19 +0100

    Debug hierarchy in playground

---
+++ Playground/LexicalPlayground/NodeHierarchyViewPlugin.swift
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+import Lexical
+import LexicalListPlugin
+import UIKit
+
+public class NodeHierarchyViewPlugin: Plugin {
+  private var _hierarchyView: UITextView
+  
+  weak var editor: Editor?
+  
+  init() {
+    self._hierarchyView = UITextView()
+    _hierarchyView.backgroundColor = .black
+    _hierarchyView.layer.borderColor = UIColor.systemGray.cgColor
+    _hierarchyView.textColor = .white
+    _hierarchyView.isEditable = false
+    _hierarchyView.isUserInteractionEnabled = true
+    _hierarchyView.isScrollEnabled = true
+    _hierarchyView.font = UIFont.monospacedSystemFont(ofSize: 12, weight: .regular)
+    _hierarchyView.showsVerticalScrollIndicator = true
+  }
+  
+  // MARK: - Plugin API
+  
+  public func setUp(editor: Editor) {
+    self.editor = editor
+    
+    _ = editor.registerUpdateListener { [weak self] activeEditorState, previousEditorState, dirtyNodes in
+      if let self {
+        self.updateHierarchyView(editorState: activeEditorState)
+      }
+    }
+  }
+  
+  public func tearDown() {
+  }
+  
+  public var hierarchyView: UIView {
+    get {
+      _hierarchyView
+    }
+  }
+  
+  // MARK: -
+  
+  private func updateHierarchyView(editorState: EditorState) {
+    do {
+      let hierarchyString = try getNodeHierarchy(editorState: editorState)
+      let selectionString = try getSelectionData(editorState: editorState)
+      _hierarchyView.text = "\(hierarchyString)\n\n\(selectionString)"
+    } catch {
+      print("Error updating node hierarchy.")
+    }
+  }
+  
+}
--- Playground/LexicalPlayground/ViewController.swift
+++ Playground/LexicalPlayground/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
   var lexicalView: LexicalView?
   weak var toolbar: UIToolbar?
+  weak var hierarchyView: UIView?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,26 +21,32 @@ class ViewController: UIViewController {
 
     let toolbarPlugin = ToolbarPlugin(viewControllerForPresentation: self)
     let toolbar = toolbarPlugin.toolbar
+    
+    let hierarchyPlugin = NodeHierarchyViewPlugin()
+    let hierarchyView = hierarchyPlugin.hierarchyView
 
     let listPlugin = ListPlugin()
 
     let theme = Theme()
     theme.indentSize = 40.0
-    let editorConfig = EditorConfig(theme: theme, plugins: [toolbarPlugin, listPlugin])
+    let editorConfig = EditorConfig(theme: theme, plugins: [toolbarPlugin, listPlugin, hierarchyPlugin])
     let lexicalView = LexicalView(editorConfig: editorConfig, featureFlags: FeatureFlags())
 
     self.lexicalView = lexicalView
     self.toolbar = toolbar
+    self.hierarchyView = hierarchyView
 
     view.addSubview(lexicalView)
     view.addSubview(toolbar)
+    view.addSubview(hierarchyView)
   }
 
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
 
-    if let lexicalView, let toolbar {
+    if let lexicalView, let toolbar, let hierarchyView {
       let safeAreaInsets = self.view.safeAreaInsets
+      let hierarchyViewHeight = 300.0
 
       toolbar.frame = CGRect(x: 0,
                              y: safeAreaInsets.top,
@@ -48,7 +55,11 @@ class ViewController: UIViewController {
       lexicalView.frame = CGRect(x: 0,
                                  y: toolbar.frame.maxY,
                                  width: view.bounds.width,
-                                 height: view.bounds.height - toolbar.frame.maxY - safeAreaInsets.bottom)
+                                 height: view.bounds.height - toolbar.frame.maxY - safeAreaInsets.bottom - hierarchyViewHeight)
+      hierarchyView.frame = CGRect(x: 0,
+                                   y: lexicalView.frame.maxY,
+                                   width: view.bounds.width,
+                                   height: hierarchyViewHeight)
     }
   }
 }
