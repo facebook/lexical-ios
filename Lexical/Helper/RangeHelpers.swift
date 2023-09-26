2412162fe60d61b4e0736caeca95ece383a7b14d
Amy Worrall <amyworrall@meta.com>
2023-09-26 12:54:33 -0700

    Function to find string ranges from within Lexical using arbitrary predicates.
    Summary:
    There are many situations where it is useful to derive string ranges from Lexical EditorState. I'm currently working on a project where I talk to an API that expects string ranges for inline styles (e.g. bold/italic). I wrote a helper function that can help derive these.
    
    Notably, I didn't assume an editor, so I couldn't read the values out of the range cache. I wanted the function to work inside an `editorState.read {}`.
    
    The algorithm is pretty simple, doing a recursive tree traversal through the children of the start node. The caller provides a closure that says whether the node in question should be included in the ranges or not. The closure can also return `.inherit`, which will include the node if it is a child of an included node.
    
    There is a version of the function that allows the closure to supply a payload, and the payloads are returned alongside the ranges.
    
    The ranges are merged if they are contiguous. If the payload version of the function is being used, the merging only happens if the payloads are also equal.
    
    Unit tests to come in a future diff.
    
    Reviewed By: zurfyx
    
    Differential Revision: D49610272
    
    fbshipit-source-id: 26cacdffb1c4a08e23d157ccc7b6a66c6929f58c

---
+++ Lexical/Helper/RangeHelpers.swift
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+
+public enum RangeSearchInclusionResult {
+  case include
+  case exclude
+  case inherit // NB inherit also inherits the payload!
+}
+
+public typealias RangePayloadPair<Payload> = (range: NSRange, payload: Payload)
+
+/**
+ Searches a Lexical subtree for nodes matching a condition, and returns the text range (plus a custom payload).
+
+ If using Lexical to interoprate with any APIs that work with attribute ranges, it is often useful to be able
+ to match up Lexical nodes with ranges.
+
+ This function mirrors some of the work done by the reconciler/range cache combo, but does not rely on the range
+ cache so that it can be used in headless/no editor situations (e.g. when doing `EditorState.read {}`).
+ */
+public func performRangeSearchWithPayload<Payload: Equatable>(searchRoot: Node, comparison: (Node) -> (RangeSearchInclusionResult, Payload)) throws -> [RangePayloadPair<Payload>] {
+  var cursor = 0
+  var ranges: [RangePayloadPair<Payload>] = []
+  try searchInNode(node: searchRoot, comparison: comparison, cursor: &cursor, ranges: &ranges, parentMatch: false, parentPayload: nil)
+
+  // merge contiguous ranges
+  return ranges.reduce([]) { mergedRanges, element in
+    guard let last = mergedRanges.last else {
+      return [element]
+    }
+    var output = mergedRanges
+    if last.range.upperBound == element.range.lowerBound && last.payload == element.payload {
+      output.removeLast()
+      let newRange = NSRange(location: last.range.location, length: element.range.upperBound - last.range.lowerBound)
+      output.append((range: newRange, payload: last.payload))
+    } else {
+      output.append(element)
+    }
+    return output
+  }
+}
+
+/**
+ Searches a Lexical subtree for nodes matching a condition, and returns the text range.
+ */
+public func performRangeSearch(searchRoot: Node, comparison: (Node) -> (RangeSearchInclusionResult)) throws -> [NSRange] {
+  let dummyPayload = true
+  let output = try performRangeSearchWithPayload(searchRoot: searchRoot, comparison: { node in
+    return (comparison(node), dummyPayload)
+  })
+  return output.map { pair in
+    return pair.range
+  }
+}
+
+// the recursive helper function for the above
+func searchInNode<Payload: Equatable>(node: Node, comparison: (Node) -> (RangeSearchInclusionResult, Payload), cursor: inout Int, ranges: inout [RangePayloadPair<Payload>], parentMatch: Bool, parentPayload: Payload?) throws {
+  let (currentMatch, currentPayload) = comparison(node)
+  let currentMatchResolved = switch currentMatch {
+  case .include: true
+  case .exclude: false
+  case .inherit: parentMatch
+  }
+  let currentPayloadResolved = (currentMatch == .inherit) ? parentPayload ?? currentPayload : currentPayload
+
+  // handle preamble and text parts
+  let start = cursor
+  cursor += node.getPreamble().lengthAsNSString()
+  cursor += node.getTextPart().lengthAsNSString()
+  if currentMatchResolved == true {
+    let range = NSRange(location: start, length: cursor - start)
+    ranges.append((range, currentPayloadResolved))
+  }
+
+  // handle children
+  if let node = node as? ElementNode {
+    for child in node.getChildren() {
+      try searchInNode(node: child, comparison: comparison, cursor: &cursor, ranges: &ranges, parentMatch: currentMatchResolved, parentPayload: currentPayloadResolved)
+    }
+  }
+
+  // handle postamble
+  let postambleStart = cursor
+  cursor += node.getPostamble().lengthAsNSString()
+  if currentMatchResolved == true {
+    let range = NSRange(location: postambleStart, length: cursor - postambleStart)
+    ranges.append((range, currentPayloadResolved))
+  }
+}
