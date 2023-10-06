facebook/lexical-ios

commit 4738571c86c80d73eb5d99397916514859da7d58

Link plugin in toolbar now works without selection (#37)
Summary:
Link plugin in toolbar now works without selection

 ---
Stack created with [Sapling](https://sapling-scm.com). Best reviewed with [ReviewStack](https://reviewstack.dev/facebook/lexical-ios/pull/37).
* __->__ https://github.com/facebook/lexical-ios/issues/37
* https://github.com/facebook/lexical-ios/issues/36

Pull Request resolved: https://github.com/facebook/lexical-ios/pull/37

Reviewed By: zurfyx

Differential Revision: D49461056

Pulled By: amyworrall

fbshipit-source-id: 076205464e002761099c58b0d68f4829899f24fd

Lexical/Core/Events.swift
@@ -157,6 +157,11 @@ func checkIfTokenOrCanTextBeInserted(node: TextNode) -> Bool {
 
 // triggered by selection change event from the UITextView
 internal func onSelectionChange(editor: Editor) {
+  if editor.isUpdating {
+    // If for example a Lexical plugin requests the text view to become first responder (i.e.
+    // after some kind of UI was presented), we don't want to overwrite the Lexical selection here.
+    return
+  }
   do {
     try editor.updateWithCustomBehaviour(mode: UpdateBehaviourModificationMode(suppressReconcilingSelection: true, suppressSanityCheck: true)) {
       let nativeSelection = editor.getNativeSelection()

Plugins/LexicalLinkPlugin/LexicalLinkPlugin/LinkPlugin.swift
@@ -84,7 +84,7 @@ open class LinkPlugin: Plugin {
 
   func toggleLink(url: String?) throws {
     guard let selection = try getSelection() else { return }
-    let nodes = try selection.extract()
+    var nodes = try selection.extract()
 
     guard let url else {
       // Remove linkNode
@@ -97,8 +97,19 @@ open class LinkPlugin: Plugin {
           try parent.remove()
         }
       }
-
       return
+    }
+
+    if nodes.count == 0 || (nodes.count == 1 && nodes.first is ElementNode) {
+      // We have a URL but no nodes. Insert a text node representing the URL.
+      let node = TextNode(text: url)
+      let result = try selection.insertNodes(nodes: [node], selectStart: false)
+      if result {
+        nodes = [node]
+      } else {
+        // could not insert node
+        return
+      }
     }
 
     // Add or merge LinkNodes

commit 3e733c7f9b81b1861d93086915d2c92047663bae

FIX for marked text, broken in PR #37

Lexical/Core/Events.swift
@@ -157,11 +157,7 @@ func checkIfTokenOrCanTextBeInserted(node: TextNode) -> Bool {
 
 // triggered by selection change event from the UITextView
 internal func onSelectionChange(editor: Editor) {
-  if editor.isUpdating {
-    // If for example a Lexical plugin requests the text view to become first responder (i.e.
-    // after some kind of UI was presented), we don't want to overwrite the Lexical selection here.
-    return
-  }
+  // Note: we have to detect selection changes here even if an update is in progress, otherwise marked text breaks!
   do {
     try editor.updateWithCustomBehaviour(mode: UpdateBehaviourModificationMode(suppressReconcilingSelection: true, suppressSanityCheck: true)) {
       let nativeSelection = editor.getNativeSelection()

Plugins/LexicalLinkPlugin/LexicalLinkPlugin/LinkPlugin.swift
@@ -71,15 +71,20 @@ open class LinkPlugin: Plugin {
 
   func insertLink(linkPayload: LinkPayload?, editor: Editor) {
     do {
+      var modifiedSelection: BaseSelection?
       try editor.update {
         getActiveEditorState()?.selection = linkPayload?.originalSelection
         try toggleLink(url: linkPayload?.urlString)
+        modifiedSelection = try getSelection()?.clone()
+      }
+      lexicalView?.textViewBecomeFirstResponder()
+      try editor.update {
+        getActiveEditorState()?.selection = modifiedSelection
       }
     } catch {
       print("\(error)")
     }
 
-    lexicalView?.textViewBecomeFirstResponder()
   }
 
   func toggleLink(url: String?) throws {