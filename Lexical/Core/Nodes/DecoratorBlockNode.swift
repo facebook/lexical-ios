170d22747ea41d398a998a2b8ed2c8e160d7450b
Nemanja Kovacevic <nemanja.kovacevic@gmail.com>
2024-11-19 22:53:03 +0100

    decorator block node

---
+++ Lexical/Core/Nodes/DecoratorBlockNode.swift
@@ -0,0 +1,45 @@
+//
+//  DecoratorBlockNode.swift
+//  Lexical
+//
+//  Created by Nemanja Kovacevic on 19.11.24..
+//
+
+import Foundation
+import UIKit
+
+extension NodeType {
+  static let block = NodeType(rawValue: "block")
+}
+
+// POC proving we can make a block (full width) decorator node
+open class DecoratorBlockNode: DecoratorNode {
+  
+  // TODO remove this, for quick testing purposes only
+  static var counter: Int = 0
+  static var next: Int {
+    counter += 1;
+    return counter
+  }
+  
+  let id: Int = DecoratorBlockNode.next
+  
+  override public class func getType() -> NodeType {
+    return .block
+  }
+  
+  override public func createView() -> UILabel {
+    let view = UILabel(frame: CGRect(origin: CGPoint.zero, size: CGSizeMake(50, 50)))
+    view.text = "Block \(id)"
+    return view
+  }
+
+  override open func decorate(view: UIView) {
+    view.backgroundColor = .lightGray
+  }
+  
+  open override func sizeForDecoratorView(textViewWidth: CGFloat, attributes: [NSAttributedString.Key : Any]) -> CGSize {
+    return CGSizeMake(textViewWidth, 50)
+  }
+  
+}
--- Lexical/Core/Selection/RangeSelection.swift
+++ Lexical/Core/Selection/RangeSelection.swift
@@ -558,7 +558,37 @@ public class RangeSelection: BaseSelection {
       }
     }
   }
-
+  
+  /// Decorator block nodes can only be direct descendants of the root element.
+  /// Anchor node can be any node, we will determine point of insertion with the help of anchor node and anchor offset
+  /// - Returns: true if node recognized and inserted, false otherwise
+  private func handleDecoratorBlockNode(_ nodes: [Node], _ anchorNode: Node, _ anchorOffset: Int) -> Bool {
+    if nodes.count == 1, let decoratorBlockNode = nodes.first as? DecoratorBlockNode {
+      guard let root = getRoot() else { return false }
+      var insertedNode: Node?
+      let parents = anchorNode.getParents()
+      switch parents.count {
+      case 0: // anchor node is root
+        if root.getChildren().count > anchorOffset {
+          try? insertedNode = root.getChildren()[anchorOffset].insertAfter(nodeToInsert: decoratorBlockNode)
+        } else {
+          try? root.append([decoratorBlockNode])
+        }
+      case 1: // anchor node is a root child, insert block after it
+        try? insertedNode = anchorNode.insertAfter(nodeToInsert: decoratorBlockNode)
+      default: // anchor node is in an element node, find a last ancestor before root and insert block after it
+        try? insertedNode = parents[parents.count-2].insertAfter(nodeToInsert: decoratorBlockNode)
+      }
+      // TODO what do we want to do with the cursor after we've inserted a block?
+      // leaving it where it was is weird (it would remain before the block)
+      // putting it right after is also weird, it would be as high as the block
+      // perhaps the most elegant solution is inserting a paragraph right below it?
+      try? insertedNode?.selectNext(anchorOffset: nil, focusOffset: nil)
+      return true
+    }
+    return false
+  }
+  
   @discardableResult
   public func insertNodes(nodes: [Node], selectStart: Bool = false) throws -> Bool {
     if !isCollapsed() {
@@ -570,6 +600,11 @@ public class RangeSelection: BaseSelection {
     let anchorNode = try anchor.getNode()
     var target = anchorNode
 
+    let handled = handleDecoratorBlockNode(nodes, anchorNode, anchorOffset)
+    if handled {
+      return true
+    }
+    
     if anchor.type == .element {
       if let element = try anchor.getNode() as? ElementNode {
         if let placementNode = element.getChildAtIndex(index: anchorOffset - 1) {
